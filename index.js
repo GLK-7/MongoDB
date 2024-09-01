6eba679f8e89a3b43513b4c95e06758492844458
Gabriel Lino <gabriel.godoitb@gmail.com>
2024-09-01 09:41:19 -0300

    create a repo git

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const conn = require("./db/conn");
+
+const productsRoutes = require("./routes/productsRoutes");
+
+// Configuração do motor de visualização
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// Middleware para ler corpo da requisição
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/products", productsRoutes);
+
+app.listen(3000, () => {
+  console.log("App rodando!");
+});
